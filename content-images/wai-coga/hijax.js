5e22caf45d2d0807e48036c0d0c3c95d2e49a3cc
Steve Lee <steve@opendirective.com>
2019-02-17 22:52:18 +0000

    Add collapse/expand

--- content-images/wai-coga/hijax.js
+++ content-images/wai-coga/hijax.js
@@ -1,31 +1,58 @@
 // Enhance the design guide with Ajax
 // NB Uses so ES6 so need a moder browser - https://caniuse.com/#search=es6 
-function getParentLocationNode(node) {
+
+function normalisePathname(pathname) {
+    const curPathname = `${window.location.origin}${window.location.pathname}`
+    const url = new URL(pathname, curPathname)
+    return url.pathname
+}
+
+function getPotentialLocationNode(node) {
     const newLocation = node.parentNode.parentNode.parentNode.firstElementChild
     return newLocation ? newLocation.className == 'page-link' ? newLocation : null : null
-}  
+}
+
+var g_navLinkNodes = []
+
+function pathnameInNavLinks(pathname) {
+    const normalPath = normalisePathname(pathname)
+    const index = g_navLinkNodes.findIndex((node) => node.getAttribute('href') == normalPath)
+    return index != -1
+}
+
+function getChildList(node) {
+    const sibling = node.nextElementSibling
+    return (sibling && sibling.nodeName == 'UL') ? sibling : null
+}
 
 function updateNavigation(newNode) {
-    const current = newNode.getAttribute('aria-current')
-
     const navList = document.getElementsByClassName('sidenav--list')[0]
+    const current = navList.querySelector(':scope a[aria-current]')
     const currentPage = navList.querySelector(':scope a[aria-current=page]')
+    const currentLocation = navList.querySelector(':scope a[aria-current=location]')
+    const potentialLocation = getPotentialLocationNode(newNode)
 
-    // how do we tell AT users about the change?
-    if (currentPage) {
-        currentPage.removeAttribute('aria-current')
-    }
-    newNode.setAttribute('aria-current', 'page')
-
-    const currentLocation = navList.querySelector(':scope > li > a[aria-current=location]')
-    const location = getParentLocationNode(newNode)
-    if (currentLocation && currentLocation != location) {
+    // Do we need tell AT users about the change?
+    currentPage.removeAttribute('aria-current')
+    if (currentLocation) {
         currentLocation.removeAttribute('aria-current')
     }
-    if (location) {
-        location.setAttribute('aria-current', 'location')
+    newNode.setAttribute('aria-current', 'page')
+    if (potentialLocation) {
+        potentialLocation.setAttribute('aria-current', 'location')
+    }
+
+    const currentChildListNode = getChildList(current)
+    const childListNode = getChildList(newNode)
+    if (currentChildListNode && !potentialLocation && currentChildListNode != childListNode) {
+        currentChildListNode.setAttribute('aria-expanded', 'false')
+    }
+    if (childListNode) {
+        const expanded = childListNode.getAttribute('aria-expanded') == 'true' ? 'false' : 'true'
+        childListNode.setAttribute('aria-expanded', expanded)
     }
 }
+
 
 async function updatePage(url) {
     const response = await fetch(url);
@@ -38,54 +65,76 @@ async function updatePage(url) {
         const main = document.querySelector('main#main')
         if (main) {
             main.innerHTML = page
-        }        
+        }
+
+        const pageLinkNodes = Array(...main.querySelectorAll('a'))
+        const linkableNodes = pageLinkNodes.filter((link) =>
+            pathnameInNavLinks(link.getAttribute('href'))
+        )
+        linkableNodes.forEach((node) => { node.addEventListener('click', onPageClick, false) })
+        console.info(`Hijaxed ${linkableNodes.length} page links`)
     }
 }
 
-function navigate(target, href) {
-    updateNavigation(target)
-    updatePage(href)
+function navigate(navNode) {
+    updateNavigation(navNode)
+    const pathname = navNode.getAttribute('href')
+    updatePage(pathname)
+
+    return pathname
 }
 
 function navigateToURL(url) {
     const target = document.querySelector(`.sidenav--list li > a[href="${url}"]`)
-    navigate(target, url)
+    navigate(target)
+}
+
+function onNavClick(event) {
+    event.preventDefault()
+
+    const target = event.currentTarget;
+    const pathname = navigate(target)
+
+    history.pushState(null, null, pathname);
+}
+
+function onPageClick(event) {
+    event.preventDefault()
+
+    const target = event.currentTarget;
+    const pathname = normalisePathname(target.getAttribute('href'))
+    navigateToURL(pathname)
+
+    history.pushState(null, null, pathname);
+}
+
+function onPopstate(event) {
+    const pathname = document.location.pathname
+    navigateToURL(pathname)
 }
 
 function hijax() {
-    function onClick(event) {
-        event.preventDefault()
+    const navLinkNodes = document.querySelectorAll('.sidenav--list li > a')
+    g_navLinkNodes = Array(...navLinkNodes)
+    navLinkNodes.forEach((node) => { node.addEventListener('click', onNavClick, false) })
+    console.info(`Hijaxed ${navLinkNodes.length} nav links`)
 
-        const target = event.currentTarget;
-        const href = target.getAttribute('href')
-        navigate(target, href)
-
-        history.pushState(null, null, href);
-    }
-
-    function onPopstate(event) {
-        const path = document.location.pathname
-        navigateToURL(path)
-    }
-
-    const navList = document.getElementsByClassName('sidenav--list')[0]
-    const locations = navList.querySelectorAll(':scope li > a')
-    locations.forEach((node) => { node.addEventListener('click', onClick, false) })
-    console.info(`Hijaxed ${locations.length} page links`)
+    navChildLists = document.querySelectorAll('.sidenav--list > li > a + ul')
+    navChildLists.forEach((node) => { node.setAttribute('aria-expanded', 'false') })
 
     window.onpopstate = onPopstate
 }
 
-var supportsES6 = function() {
+function supportsES6() {
     try {
-      new Function("(a = 0) => a");
-      return true;
+        new Function("(a = 0) => a");
+        return true;
     }
     catch (err) {
-      return false;
+        return false;
     }
-  }()
-  
-if (supportsES6) {
+}
+
+if (supportsES6()) {
     window.addEventListener("load", hijax)
 }
